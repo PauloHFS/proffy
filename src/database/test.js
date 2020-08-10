37e3b454c77e70bdce2546b450146aebc681ba9f
Paulo Hernane <paulohernane10@gmail.com>
2020-08-10 18:43:38 -0300

    database

--- src/database/test.js
+++ src/database/test.js
@@ -34,17 +34,32 @@ Database.then(async db => {
     //await createProffy(db, {proffyValue, classValue, classScheduleValues});
 
     // Consultar os dados inseridos
-    
+
     //todos os proffys
     const selectedProffys = await db.all("SELECT * FROM proffys");
-    console.log(selectedProffys);
+    /* console.log(selectedProffys); */
 
     // consultar as classes de um determinado professor
     // e trazer junto os dados do professor
     const selectClassesAndProffys = await db.all(`
         SELECT classes.*, proffys.*
         FROM proffys
-        JOIN classes ON (classes.proffy_id = proffy.id)
+        JOIN classes ON (classes.proffy_id = proffys.id)
         WHERE classes.proffy_id = 1;
     `);
+    /* console.log(selectClassesAndProffys); */
+
+    //  o horário que a pessoa trabalha, por exemplo, é das 8h  - 18h
+    //  o horário do time_from (8h) precisa ser antes ou igual ao horário solicitado
+    // o time_to precisa ser acima
+    const selectClassesSchedules = await db.all(`
+        SELECT class_schedule.*
+        FROM class_schedule
+        WHERE class_schedule.class_id = 1
+        AND class_schedule.weekday = "0"
+        AND class_schedule.time_from <= "520"
+        AND class_schedule.time_to > "520";
+    `);
+
+    console.log(selectClassesSchedules);
 });
---
+++ src/pages.js
@@ -0,0 +1,82 @@
+const Database = require('./database/db');
+
+const {
+    subjects,
+    weekdays,
+    getSubject,
+    convertHoursToMinutes
+} = require('./utils/format');
+
+function pageLanding(req, res) {
+    return res.render("index.html");
+}
+
+async function pageStudy(req, res) {
+    const filters = req.query;
+
+    if (!filters.subject || !filters.weekdays || !filters.time) {
+        return res.render("study.html", {
+            filters,
+            subjects,
+            weekdays
+        });
+    }
+
+    // converter horas em minutos
+    const timeToMinutes = convertHoursToMinutes(filters.time);
+    const query = `
+        SELECT classes.*, proffys.*
+        FROM proffys
+        JOIN classes ON (classes.proffy_id = proffys.id)
+        WHERE EXISTS (
+            SELECT class_schedule.*
+            FROM class_schedule
+            WHERE class_schedule.class_id = classes.id
+            AND class_schedule.weekday = ${filters.weekdays}
+            AND class_schedule.time_from <= ${timeToMinutes}
+            AND class_schedule.time_to > ${timeToMinutes};
+        )
+        AND classes.subject = '${filter.subject}'
+    `;
+
+    //caso haja erro na hora da consulta do banco de dados
+    try {
+        const db = await Database;
+        const proffys = await db.all(query);
+        console.log(proffys);
+        return res.render('study.html', {
+            proffys,
+            subjects,
+            filters,
+            weekdays
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function pageGiveClasses(req, res) {
+    const data = req.query;
+
+    // [name, avatar, ...]
+    const isNotEmpty = Object.keys(data).length != 0;
+    //adicionar data a lista de proffys
+    if (isNotEmpty) {
+        data.subject = getSubject(data.subject);
+
+        proffys.push(data);
+
+        return res.redirect("/study");
+    }
+
+    return res.render("give-classes.html", {
+        subjects,
+        weekdays
+    });
+}
+
+module.exports = {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses
+}
